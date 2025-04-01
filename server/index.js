c7ea68a07a4894d33f03a2bb2bd85f6fd9395e44
skiran336 <163210382+skiran336@users.noreply.github.com>
2025-04-01 04:07:53 -0400

    remove server

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('./app');
-
-const handler = serverless(app);
-
-module.exports.handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  return await handler(event, context);
-};
--- server/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { syncUser } = require('../controllers/userController');
-
-router.post('/sync', syncUser);
-
-module.exports = router;
