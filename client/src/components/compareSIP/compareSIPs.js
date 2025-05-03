c0e05fdcc227a330f01668a068474a3a94c821b4
skiran336 <163210382+skiran336@users.noreply.github.com>
2025-05-03 14:47:16 -0400

    final 10

--- client/src/components/compareSIP/compareSIPs.js
+++ client/src/components/compareSIP/compareSIPs.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { supabase } from '../../services/supabaseClient';
 import '../../styles/Compare.css';
 
