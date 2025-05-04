2f84c5172bb746fdf3e5fbca18a9dba424368bb1
skiran336 <163210382+skiran336@users.noreply.github.com>
2025-05-04 13:05:47 -0400

    final 38

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import { useNavigate, Link } from 'react-router-dom';
 import { FiLogIn, FiLoader, FiEye, FiEyeOff } from 'react-icons/fi';
+import HCaptcha from '@hcaptcha/react-hcaptcha';
 import '../../styles/Login.css';
 
 export default function Login() {
@@ -9,20 +10,32 @@ export default function Login() {
   const [password, setPassword] = useState('');
   const { signIn, loading, error } = useAuth();
   const [showPassword, setShowPassword] = useState(false);
+  const [token, setToken] = useState(null);
+  const captchaRef = useRef(null);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!token) {
+      alert('Please complete the captcha');
+      return;
+    }
     try {
-      await signIn({ email, password });
+      await signIn({ email, password, token });
       navigate('/home');
     } catch (error) {
       console.error('Login failed:', error);
+      captchaRef.current.resetCaptcha();
+      setToken(null);
     }
   };
   
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
+  };
+
+  const onVerify = (token) => {
+    setToken(token);
   };
 
   return (
@@ -75,9 +88,17 @@ export default function Login() {
             </button>
           </div>
 
+          <div className="captcha-container">
+            <HCaptcha
+              sitekey={process.env.REACT_APP_HCAPTCHA_SITE_KEY}
+              onVerify={onVerify}
+              ref={captchaRef}
+            />
+          </div>
+
           <button 
             type="submit" 
-            disabled={loading}
+            disabled={loading || !token}
             className={`login-button ${loading ? 'loading' : ''}`}
           >
             {loading ? (
